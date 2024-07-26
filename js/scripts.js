e4a15af3cdaf65244a70580c72321a25858d2f5f
Çağan Arda Özkan <caganardaozkan@hotmail.com>
2024-07-26 15:20:29 +0100

    added some effects and styles and more projects

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,17 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+document.addEventListener('DOMContentLoaded', function() {
+    const projectCards = document.querySelectorAll('.card');
+
+    projectCards.forEach(card => {
+        card.addEventListener('mouseover', function() {
+            card.classList.add('card-hover-effect');
+        });
+
+        card.addEventListener('mouseout', function() {
+            card.classList.remove('card-hover-effect');
+        });
+    });
+});
